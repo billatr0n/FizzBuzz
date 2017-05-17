d92bbc78fd12b5bab8861210377d3e0f0fc1ab83
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-17 09:30:08 +0200

    Add first failing Test

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,8 +1,24 @@
 import org.junit.Assert;
 import org.junit.Test;
+
 /**
  * Created by Bill on 17/5/2017.
  */
 
 public class FizzBuzzTest {
+
+    @Test
+    public void provideNumber_when_gets_one_answers_one() {
+        //expected
+        final String expected = "1";
+
+        //act
+        FizzBuzz myFz = new FizzBuzz();
+        final String actual = myFz.provideNumber(expected);
+
+
+        //Assert
+        Assert.assertEquals("Expected value is 1", expected, actual);
+
+    }
 }
