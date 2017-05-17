58d62ab6504567619e2c1c1363a31f05a00bbb34
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-17 09:46:21 +0200

    Extends implemenetation for BUZZ

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -5,11 +5,16 @@ public class FizzBuzz {
     public static String FIZZ = "fizz";
     public static int FIZZ_DIVIDER = 3;
 
+    public static String BUZZ = "buzz";
+    public static int BUZZ_DIVIDER = 5;
+
     public String provideNumber(Integer number){
         String result = number.toString();
 
         if(isDividable(number,FIZZ_DIVIDER))
             result = FIZZ;
+        else if(isDividable(number,BUZZ_DIVIDER))
+            result = BUZZ;
 
         return result;
     }
