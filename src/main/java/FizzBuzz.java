a6dbc38f867ad309b1fa3b2012c03122e3e12617
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-17 09:23:41 +0200

    Initial Commit. Default Branch

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Bill on 17/5/2017.
+ */
+public class FizzBuzz {
+}
+
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,8 @@
+import org.junit.Assert;
+import org.junit.Test;
+/**
+ * Created by Bill on 17/5/2017.
+ */
+
+public class FizzBuzzTest {
+}
