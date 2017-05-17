48bd71cbd1b38c06ad4aa77dfe22fd2cdfc16cc4
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-17 09:34:23 +0200

    Add first implementation, and fix unit tests to pass

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,5 +2,10 @@
  * Created by Bill on 17/5/2017.
  */
 public class FizzBuzz {
+
+    public String provideNumber(Integer number){
+        return number.toString();
+    }
+
 }
 
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -9,12 +9,14 @@ public class FizzBuzzTest {
 
     @Test
     public void provideNumber_when_gets_one_answers_one() {
+        final int providedNumber = 1;
+
         //expected
         final String expected = "1";
 
         //act
-        FizzBuzz myFz = new FizzBuzz();
-        final String actual = myFz.provideNumber(expected);
+        FizzBuzz SUT = new FizzBuzz();
+        final String actual = SUT.provideNumber(providedNumber);
 
 
         //Assert
