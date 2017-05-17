35f82c32d63747671cce2f394958422be80585bd
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-17 09:36:01 +0200

    Add unit tests for dividing by 3

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -23,4 +23,21 @@ public class FizzBuzzTest {
         Assert.assertEquals("Expected value is 1", expected, actual);
 
     }
+
+    @Test
+    public void provideNumber_when_gets_dividable_by_three_answers_fizz() {
+        final int providedNumber = 3;
+
+        //expected
+        final String expected = "fizz";
+
+        //act
+        FizzBuzz SUT = new FizzBuzz();
+        final String actual = SUT.provideNumber(providedNumber);
+
+
+        //Assert
+        Assert.assertEquals("Expected value is fizz", expected, actual);
+
+    }
 }
