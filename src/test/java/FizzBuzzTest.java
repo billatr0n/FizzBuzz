9ed0d09c7657a16636da6ec217fb17b7298c874b
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-18 09:51:47 +0200

    Refactor unit tests to implement parameterized tests

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,26 +1,50 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by Bill on 17/5/2017.
  */
 
+@RunWith(JUnitParamsRunner.class)
 public class FizzBuzzTest {
+    FizzBuzz SUT;
+
+    private static final String EXPECTED_ONE = "1";
+    private static final String EXPECTED_TWO = "2";
+
+    private static final int POINTED_WITH_ONE = 1;
+    private static final int POINTED_WITH_TWO = 2;
+
+    @Before
+    public void setup() {
+
+        SUT = new FizzBuzz();
+    }
+
+    //In gogle drive there is a good book that explain this practical unit testing with junit and mockito
+    private static final Object[] getPointedNumbersAndSayNumbers() {
+
+        return new Object[]{
+
+                new Object[]{POINTED_WITH_ONE, EXPECTED_ONE},
+                new Object[]{POINTED_WITH_TWO, EXPECTED_TWO},
+        };
+    }
+
 
     @Test
-    public void provideNumber_when_gets_one_answers_one() {
-        //Arrange
-        final int providedNumber = 1;
-
-        //expected
-        final String expected = "1";
+    @Parameters(method = "getPointedNumbersAndSayNumbers")
+    public void provideNumber_when_gets_one_answers_one(int givenNumber, String expectedString) {
 
         //act
-        FizzBuzz SUT = new FizzBuzz();
-        final String actual = SUT.provideNumber(providedNumber);
+        final String actual = SUT.provideNumber(givenNumber);
 
         //Assert
-        Assert.assertEquals("Expected value is 1", expected, actual);
+        Assert.assertEquals("Expected value is " + expectedString, expectedString, actual);
 
     }
 }
