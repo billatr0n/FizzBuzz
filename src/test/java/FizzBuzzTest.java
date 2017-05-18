c560f2c347b2f86f57a059137ca062fc3207e08b
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-18 10:34:18 +0200

    Add failing tests for NotANumber

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -18,6 +18,7 @@ public class FizzBuzzTest {
     private static final String EXPECTED_FIZZ = "fizz";
     private static final String EXPECTED_BUZZ = "buzz";
     private static final String EXPECTED_FIZZBUZZ = "fizzbuzz";
+    private static final String EXPECTED_NotANumber = "This is not a number";
 
     private static final int POINTED_WITH_ONE = 1;
     private static final int POINTED_WITH_TWO = 2;
@@ -25,6 +26,7 @@ public class FizzBuzzTest {
     private static final int POINTED_WITH_FIVE = 5;
     private static final int POINTED_WITH_FIFTEEN = 15;
     private static final int POINTED_WITH_SIXTY = 60;
+    private static final String POINTED_WITH_A_GUN = "This is a gun";
 
     @Before
     public void setup() {
@@ -56,6 +58,16 @@ public class FizzBuzzTest {
 
         //Assert
         Assert.assertEquals("Expected value is " + expectedString, expectedString, actual);
+
+    }
+    @Test
+    public void provideNumber_When_Gets_NonInteger_Answers_NotANumber(){
+
+        //act
+        final String actual = SUT.provideNumber(POINTED_WITH_A_GUN);
+
+        //Assert
+        Assert.assertEquals(EXPECTED_NotANumber,EXPECTED_NotANumber, actual);
 
     }
 }
