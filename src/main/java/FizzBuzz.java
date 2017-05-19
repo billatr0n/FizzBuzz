f1189a41d71896a78d75152022e98a8944e40ad4
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-19 16:42:58 +0200

    Initial implementation for FIZZ

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,8 +2,23 @@
  * Created by Bill on 17/5/2017.
  */
 public class FizzBuzz {
+    public static final Integer FIZZ_VALUE = 3;
+    public static final String FIZZ_STR = "fizz";
+
     public String provideNumber(int providedNumber) {
-        return String.valueOf(providedNumber);
+        String result = String.valueOf(providedNumber);
+        if(isFizz(providedNumber)){
+            result = FIZZ_STR;
+        }
+        return result;
+    }
+
+    public boolean isFizz(int provideNumber) {
+        boolean result = false;
+        if (Math.floorMod(provideNumber, FIZZ_VALUE) == 0) {
+            return true;
+        }
+        return result;
     }
 }
 
