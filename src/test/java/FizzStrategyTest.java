ab71b3af82e43c2c0c6dedd5e915e984c4d61f17
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-19 16:52:59 +0200

    Refactoring Step 1, introduce failing unit tests for FizzStrategy

---
+++ src/test/java/FizzStrategyTest.java
@@ -0,0 +1,43 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by vasilis on 19-5-17.
+ */
+
+@RunWith(JUnitParamsRunner.class)
+public class FizzStrategyTest {
+
+    FizzStrategy SUT;
+
+    private static final String EXPECTED_FIZZ = "fizz";
+
+    private static final int POINTED_WITH_THREE = 3;
+
+    private static final int POINTED_WITH_SIX = 6;
+
+
+    private static final Object[] getPointedNumbersAndSayNumbers() {
+
+        return new Object[]{
+                new Object[]{POINTED_WITH_THREE, EXPECTED_FIZZ},
+                new Object[]{POINTED_WITH_SIX, EXPECTED_FIZZ},
+        };
+    }
+
+
+    @Test
+    @Parameters(method = "getPointedNumbersAndSayNumbers")
+    public void provideNumber_When_Gets_Number_Answers_ExpectedValue(int givenNumber, String expectedString) {
+
+        //act
+        final String actual = SUT.provideNumber(givenNumber);
+
+        //Assert
+        Assert.assertEquals("Expected value is " + expectedString, expectedString, actual);
+
+    }
+}
