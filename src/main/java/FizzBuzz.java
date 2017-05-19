f34e0debd89ff2597fba8fc0f8e3c6b0d53c5cd3
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-19 16:49:18 +0200

    Initial implementation for FIZZBUZZ

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -6,14 +6,19 @@ public class FizzBuzz {
     public static final String FIZZ_STR = "fizz";
     public static final Integer BUZZ_VALUE = 5;
     public static final String BUZZ_STR = "buzz";
+    public static final Integer FIZZ_BUZZ_VALUE = 15;
+    public static final String FIZZ_BUZZ_STR = "fizzbuzz";
 
     public String provideNumber(int providedNumber) {
         String result = String.valueOf(providedNumber);
-        if(isFizz(providedNumber)){
+        if(isFizzBuzz(providedNumber)){
+            result = FIZZ_BUZZ_STR;
+        }else if(isFizz(providedNumber)){
             result = FIZZ_STR;
         }else if(isBuzz(providedNumber)){
             result = BUZZ_STR;
         }
+
         return result;
     }
 
@@ -28,6 +33,13 @@ public class FizzBuzz {
     public boolean isBuzz(int provideNumber) {
         boolean result = false;
         if (Math.floorMod(provideNumber, BUZZ_VALUE) == 0) {
+            return true;
+        }
+        return result;
+    }
+    public boolean isFizzBuzz(int provideNumber) {
+        boolean result = false;
+        if (Math.floorMod(provideNumber, FIZZ_BUZZ_VALUE) == 0) {
             return true;
         }
         return result;
