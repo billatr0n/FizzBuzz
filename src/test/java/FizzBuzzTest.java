65ac7da71b409c40b5c0966c2d355f9ea6c44508
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-19 16:33:27 +0200

    Initial Failing test for numbers

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -5,4 +5,20 @@ import org.junit.Test;
  */
 
 public class FizzBuzzTest {
+    @Test
+    public void provideNumber_when_gets_number_answers_expectedValue() {
+        final int providedNumber = 1;
+
+        //expected
+        final String expected = "1";
+
+        //act
+        FizzBuzz SUT = new FizzBuzz();
+        final String actual = SUT.provideNumber(providedNumber);
+
+
+        //Assert
+        Assert.assertEquals("Expected value is 1", expected, actual);
+
+    }
 }
